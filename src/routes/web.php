3b766369f800ef396e3e1463c7145ff7f87201a2
hikkymouse1007 <mc.mdt1007it@gmail.com>
2021-03-08 23:45:45 +0900

    update

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello ' . config('app.word');
 });
